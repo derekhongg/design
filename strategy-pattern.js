918654e677f4cd134b2d0aea612f07f435f5afd4
derekhongg <hongderek@gmail.com>
2022-08-26 22:08:47 -0700

    new pattern added

---
+++ strategy-pattern.js
@@ -0,0 +1,60 @@
+/* Strategy Pattern
+
+- You encapsulate a group or family of closely related algorithms
+- Stategies are functions
+- Allows you to swap strategies easily 
+
+*/
+
+
+function Fedex() {
+    this.calculate = package => {
+        // random fedex calculations...
+        return 2.45
+    }
+}
+
+function UPS() {
+    this.calculate = package => {
+        //random ups calculations...
+        return 1.56
+    }
+}
+
+function USPS(){
+    this.calculate = package => {
+        // random usps calculations...
+        return 4.5
+    }
+}
+
+function Shipping(){
+    this.company= ""
+    this.setStrategy = (company) => {
+        this.company = company;
+    }
+    this.calculate = package => {
+        return this.company.calculate(package)
+    }
+}
+
+const fedex = new Fedex()
+const ups = new UPS()
+const usps = new USPS()
+
+const package = {
+    from: "Alabama",
+    to: "Georgia",
+    weight:1.56
+}
+
+const shipping = new Shipping();
+shipping.setStrategy(fedex)
+console.log("Fedex: " + shipping.calculate(package))
+
+
+shipping.setStrategy(ups)
+console.log("UPS: " + shipping.calculate(package))
+
+shipping.setStrategy(usps)
+console.log("USPS: " + shipping.calculate(package))
